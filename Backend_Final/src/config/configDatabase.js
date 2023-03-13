10c6a2d91af01e767b83dcb5df9a524e0bab2045
Jayson Nguyen <buleberry.nguyen@gmail.com>
2023-03-13 11:35:10 +0700

    fix file config database

--- Backend_Final/src/config/configDatabase.js
+++ Backend_Final/src/config/configDatabase.js
@@ -3,7 +3,7 @@ const config = {
     password: "Thanhnguyen1", // better stored in an app setting such as process.env.DB_PASSWORD
     server: "jaysonguyendb.database.windows.net", // better stored in an app setting such as process.env.DB_SERVER
     port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
-    database: "test", // better stored in an app setting such as process.env.DB_NAME
+    database: "mobileSystemDB", // better stored in an app setting such as process.env.DB_NAME
     authentication: {
       type: "default",
     },
