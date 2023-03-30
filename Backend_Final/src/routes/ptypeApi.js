66fcedd6c2a2f645498c7e3647ed69297b123039
AnhQuan <daungocanhquan@gmail.com>
2023-03-30 09:07:18 +0700

    delete PtypeOwn

--- Backend_Final/src/routes/ptypeApi.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getallptype, createoneptype, deleteptype, updateptype, getoneptype } = require("../controller/ptypeController");
-router.route("/findall").get(getallptype);
-router.route("/create").post(createoneptype);
-router.route("/delete").delete(deleteptype);
-router.route("/update").put(updateptype);
-router.route("/findone/:id").get(getoneptype);
-module.exports = router;
