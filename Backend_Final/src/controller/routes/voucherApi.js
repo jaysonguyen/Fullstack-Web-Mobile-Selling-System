7c0fcb86c1b62b4e1a7543ec53c47d5548f59caf
AnhQuan <daungocanhquan@gmail.com>
2023-04-04 11:18:44 +0700

    fixed

--- Backend_Final/src/controller/routes/voucherApi.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getallvoucher, createvoucher, deletevoucher, updatevoucher, getonevoucher} = require("../controller/VoucherController");
-router.route("/findall").get(getallvoucher);
-router.route("/create").post(createvoucher);
-router.route("/delete").delete(deletevoucher);
-router.route("/update").put(updatevoucher);
-router.route("/findone/:id").get(getonevoucher);
-module.exports = router;
